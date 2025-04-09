7b5f5cf27a6289e5b0ce94245443dc882c767947
dcardona10 <david.cardona.410@gmail.com>
2025-04-08 19:54:08 -0500

    Clase 07
    Clase 07

---
+++ cl07/Calc.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class Calc {
+    private JPanel pnlMain;
+    private JTextField txtScreen;
+    private JButton btn1;
+    private JButton btn2;
+    private JButton btn3;
+    private JButton btn4;
+    private JButton btn5;
+    private JButton btn6;
+    private JButton btn7;
+    private JButton btn8;
+    private JButton btn9;
+    private JButton btnAC;
+    private JButton btn0;
+    private JButton btnEq;
+    private JButton btnSum;
+    private JButton btnSqrt;
+    private JButton btnSub;
+    private JButton btnMul;
+    private JButton btnDiv;
+    private JButton btnPow2;
+    private JButton btnPow3;
+    private JButton btnPowN;
+}
