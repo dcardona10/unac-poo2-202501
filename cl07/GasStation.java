99f59e19ea63a7488a142096bb33ca0dd18ba995
dcardona10 <david.cardona.410@gmail.com>
2025-05-05 17:59:34 -0500

    Clase 08
    Clase 08

---
+++ cl07/GasStation.java
@@ -0,0 +1,102 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
+public class GasStation {
+    private JPanel pnlMain;
+    private JLabel lblPrices;
+    private JLabel lblRegular;
+    private JLabel lblPremium;
+    private JTextField txtRegular;
+    private JTextField txtPremium;
+    private JRadioButton rbtnEnableEdit;
+    private JButton btnSave;
+    private JLabel lblService;
+    private JLabel lblFuelType;
+    private JRadioButton rbtnRegular;
+    private JRadioButton rbtnPremium;
+    private JLabel lblGallons;
+    private JTextField txtGallons;
+    private JButton btnFinish;
+    private JLabel lblTotal;
+    private JTextField txtTotal;
+
+    private double regularPrice = 0;
+    private double premiumPrice = 0;
+    private double gallons = 0;
+    private double total = 0;
+    private int fuelType = 0;
+
+    public JPanel getPnlMain() {
+        return pnlMain;
+    }
+
+    public GasStation() {
+
+        ButtonGroup fuelTypes = new ButtonGroup();
+        fuelTypes.add(rbtnRegular);
+        fuelTypes.add(rbtnPremium);
+
+        rbtnEnableEdit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtRegular.setEnabled(true);
+                txtPremium.setEnabled(true);
+            }
+        });
+
+        btnSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    regularPrice = Double.valueOf(txtRegular.getText());
+                    premiumPrice = Double.valueOf(txtPremium.getText());
+                    txtRegular.setText(String.format("%.2f%n", regularPrice).replace(",", "."));
+                    txtPremium.setText(String.format("%.2f%n", premiumPrice).replace(",", "."));
+                    rbtnEnableEdit.setSelected(false);
+                    txtRegular.setEnabled(false);
+                    txtPremium.setEnabled(false);
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, "Error", "Please enter a valid value", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+
+        rbtnRegular.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fuelType = 1;
+            }
+        });
+
+        rbtnPremium.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fuelType = 2;
+            }
+        });
+
+        btnFinish.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    gallons = Double.valueOf(txtGallons.getText());
+                    switch (fuelType) {
+                        case 1:
+                            total = gallons * regularPrice;
+                            break;
+                        case 2:
+                            total = gallons * premiumPrice;
+                            break;
+                        default:
+                            break;
+                    }
+                    txtTotal.setText(String.format("%.2f%n", total).replace(",", "."));
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, "Error", "Please enter a valid value", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+    }
+}
--- cl07/Main.java
+++ cl07/Main.java
@@ -1,2 +1,14 @@
+import javax.swing.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(new GasStation().getPnlMain());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
